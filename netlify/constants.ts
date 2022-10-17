349b6b545d6034d734f119bb48abeb61e4a8860c
Sim Boon Long <simboonlong@gmail.com>
2022-10-17 09:22:18 +0800

    fix: cors tt

--- netlify/constants.ts
+++ netlify/constants.ts
@@ -4,6 +4,8 @@ export const ALLOWED_ORIGINS = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
   "https://creator.tradetrust.io",
+  "https://dev.tradetrust.io",
+  "https://tradetrust.io",
 ];
 
 export enum ERROR_MESSAGE {
