696876bbb270982f4ce24ba539440b7fc06147b2
Sim Boon Long <simboonlong@gmail.com>
2022-12-14 09:43:39 +0800

    refactor: headers test

--- tests/integration/document-storage.test.ts
+++ tests/integration/document-storage.test.ts
@@ -6,22 +6,6 @@ const API_ENDPOINT = "http://localhost:9999/.netlify/functions/storage";
 const request = supertest(API_ENDPOINT);
 const postData = { document: documentGoerli };
 
-describe("API key check", () => {
-  it("should fail with 400 when API key is not provided", async () => {
-    const response = await request.post("/").send(postData).expect(400);
-    expect(response.text).toBe(ERROR_MESSAGE.API_KEY_INVALID);
-  });
-
-  it("should fail with 400 when incorrect API key is provided", async () => {
-    const response = await request
-      .post("/")
-      .set("x-api-key", "incorrectKey")
-      .send(postData)
-      .expect(400);
-    expect(response.text).toBe(ERROR_MESSAGE.API_KEY_INVALID);
-  });
-});
-
 describe("POST /", () => {
   it("should store encrypted document", async () => {
     const response = await request
@@ -106,43 +90,5 @@ describe("POST /:id", () => {
     expect(response.body).toHaveProperty("key", queueResponse.body.key); // important to check!
     expect(response.body).toHaveProperty("type", "OPEN-ATTESTATION-TYPE-1");
     expect(Object.keys(response.body).length).toBe(3);
-  });
-});
-
-describe("cors", () => {
-  it("should fail with 500 when origin is unallowed", async () => {
-    await request
-      .post("/")
-      .set("x-api-key", process.env.API_KEY)
-      .set("Origin", "http://foobar.com")
-      .send(postData)
-      .expect(500);
-  });
-
-  it("should pass with 200 when origin is TradeTrust creator", async () => {
-    await request
-      .post("/")
-      .set("x-api-key", process.env.API_KEY)
-      .set("Origin", "https://creator.tradetrust.io")
-      .send(postData)
-      .expect(200);
-  });
-
-  it("should pass with 200 when origin is TradeTrust testnets website", async () => {
-    await request
-      .post("/")
-      .set("x-api-key", process.env.API_KEY)
-      .set("Origin", "https://dev.tradetrust.io")
-      .send(postData)
-      .expect(200);
-  });
-
-  it("should pass with 200 when origin is TradeTrust production website", async () => {
-    await request
-      .post("/")
-      .set("x-api-key", process.env.API_KEY)
-      .set("Origin", "https://tradetrust.io")
-      .send(postData)
-      .expect(200);
   });
 });
