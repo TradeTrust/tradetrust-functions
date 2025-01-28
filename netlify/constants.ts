586fb00365e794957aff2bde3fb85265adc88c47
rongquan1 <85145303+rongquan1@users.noreply.github.com>
2025-01-28 12:20:25 +0800

    fix: allow new ref site (#63)

--- netlify/constants.ts
+++ netlify/constants.ts
@@ -13,6 +13,7 @@ export const ALLOWED_ORIGINS = [
   "https://dev.tradetrust.io",
   "https://tradetrust.io",
   "https://www.tradetrust.io",
+  "https://ref.tradetrust.io",
 ];
 
 export enum ERROR_MESSAGE {
