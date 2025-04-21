86e65ed396d8aacf78d48c3d9d1add14e9088c4e
maxufeng <maxufeng@caict.ac.cn>
2025-04-21 12:12:27 +0800

    fix: Repair the document document-astrontestnet-storage.test.ts

--- tests/integration/document-astrontestnet-storage.test.ts
+++ tests/integration/document-astrontestnet-storage.test.ts
@@ -1,74 +1,161 @@
 import supertest from "supertest";
-import documentMainnetV2 from "../fixtures/v2/document-mainnet.json";
 import documentAstronV2 from "../fixtures/v2/document-astrontestnet.json";
 import documentAstronV3 from "../fixtures/v3/document-astrontestnet.json";
-import { ERROR_MESSAGE } from "../../netlify/constants";
+import documentAstronNoNetworkV2 from "../fixtures/v2/document-astrontestnet-no-network.json";
+import documentAstronNoNetworkV3 from "../fixtures/v3/document-astrontestnet-no-network.json";
 
-const RESPONSE_VERIFY_SUCCESS_SUMMARY = {
-  all: true,
-  documentStatus: true,
-  documentIntegrity: true,
-  issuerIdentity: true,
-};
+import {
+  ERROR_MESSAGE,
+  DOCUMENT_STORAGE_ERROR_MESSAGE,
+} from "../../netlify/constants";
 
-const API_ENDPOINT = "http://localhost:9999/.netlify/functions/verify";
+const API_ENDPOINT = "http://localhost:9999/.netlify/functions/storage";
 const request = supertest(API_ENDPOINT);
-const postDataMainnetV2 = { document: documentMainnetV2 };
 const postDataAstronV2 = { document: documentAstronV2 };
 const postDataAstronV3 = { document: documentAstronV3 };
 
+const csrfToken = "mock-csrf-token"; // Mocked CSRF token
+const csrfTokenCookie = `csrfToken=${csrfToken}; HttpOnly; Path=/; SameSite=Strict`; // Mocked CSRF cookie
+
 describe("POST /", () => {
-  it("should verify a mainnet document by default", async () => {
+  it("should store encrypted v2 astrontestnet document", async () => {
     const response = await request
       .post("/")
-      .send(postDataMainnetV2)
-      .expect(200);
-    expect(response.body.summary).toStrictEqual(
-      RESPONSE_VERIFY_SUCCESS_SUMMARY,
-    );
-  });
-  it("should not verify a astrontestnet document by default", async () => {
-    const response = await request.post("/").send(postDataAstronV2).expect(400);
-    expect(response.body.message).toBe(ERROR_MESSAGE.DOCUMENT_GENERIC_ERROR);
-  });
-  it("should verify a v2 astrontestnet document with astrontestnet network query", async () => {
-    const response = await request
-      .post("/")
-      .query({ network: "astrontestnet" })
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
       .send(postDataAstronV2)
       .expect(200);
-    expect(response.body.summary).toStrictEqual(
-      RESPONSE_VERIFY_SUCCESS_SUMMARY,
-    );
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("key");
+    expect(response.body).toHaveProperty("type", "OPEN-ATTESTATION-TYPE-1");
+    expect(Object.keys(response.body).length).toBe(3);
   });
-  it("should verify a v3 astrontestnet document with astrontestnet network query", async () => {
+
+  it("should store encrypted v3 astrontestnet document", async () => {
     const response = await request
       .post("/")
-      .query({ network: "astrontestnet" })
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
       .send(postDataAstronV3)
       .expect(200);
-    expect(response.body.summary).toStrictEqual(
-      RESPONSE_VERIFY_SUCCESS_SUMMARY,
-    );
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("key");
+    expect(response.body).toHaveProperty("type", "OPEN-ATTESTATION-TYPE-1");
+    expect(Object.keys(response.body).length).toBe(3);
   });
-  it("should not verify a astrontestnet document with unsupported network query", async () => {
+
+  it("should throw error when document is not compliant to OA schema", async () => {
     const response = await request
       .post("/")
-      .query({ network: "foo" })
-      .send(postDataAstronV2)
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .send({
+        document: { foo: "bar" },
+      })
       .expect(400);
-    expect(response.body.message).toStrictEqual(
-      ERROR_MESSAGE.NETWORK_UNSUPPORTED,
-    );
+
+    expect(response.body.message).toBe(ERROR_MESSAGE.DOCUMENT_SCHEMA_INVALID);
   });
-  it("should verify a astrontestnet document", async () => {
+
+  it("should throw error when v2 document's network field does not exists", async () => {
     const response = await request
       .post("/")
-      .query({ network: "astrontestnet" })
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .send({
+        document: documentAstronNoNetworkV2,
+      })
+      .expect(400);
+
+    expect(response.body.message).toBe(
+      ERROR_MESSAGE.DOCUMENT_NETWORK_NOT_FOUND,
+    );
+  });
+  it("should throw error when v3 document's network field does not exists", async () => {
+    const response = await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .send({
+        document: documentAstronNoNetworkV3,
+      })
+      .expect(400);
+
+    expect(response.body.message).toBe(
+      ERROR_MESSAGE.DOCUMENT_NETWORK_NOT_FOUND,
+    );
+  });
+});
+
+describe("GET /queue", () => {
+  it("should retrieve s3 object id + oa encryption key", async () => {
+    const response = await request
+      .get("/queue")
+      .set("x-api-key", process.env.API_KEY)
+      .expect(200);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("key");
+    expect(Object.keys(response.body).length).toBe(2);
+  });
+});
+
+describe("GET /:id", () => {
+  it("should retrieve encrypted document without key", async () => {
+    const postResponse = await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
       .send(postDataAstronV2)
       .expect(200);
-    expect(response.body.summary).toStrictEqual(
-      RESPONSE_VERIFY_SUCCESS_SUMMARY,
+
+    const getResponse = await request
+      .get(`/${postResponse.body.id}`)
+      .expect(200);
+
+    expect(getResponse.body).toHaveProperty("cipherText");
+    expect(getResponse.body).toHaveProperty("iv");
+    expect(getResponse.body).toHaveProperty("tag");
+    expect(getResponse.body).toHaveProperty("type", "OPEN-ATTESTATION-TYPE-1");
+    expect(getResponse.body).not.toHaveProperty("key"); // important to check!
+    expect(Object.keys(getResponse.body).length).toBe(4);
+  });
+
+  it("should fail if id is not found in s3 objects", async () => {
+    const response = await request.get("/abc").expect(400);
+
+    expect(response.body.message).toBe(
+      DOCUMENT_STORAGE_ERROR_MESSAGE.KEY_NOT_EXISTS,
     );
+  });
+});
+
+describe("POST /:id", () => {
+  it("store a new encrypted document, replacing specified id from previous queue while using the retrieved decrypt key", async () => {
+    const queueResponse = await request
+      .get("/queue")
+      .set("x-api-key", process.env.API_KEY)
+      .expect(200);
+
+    const response = await request
+      .post(`/${queueResponse.body.id}`)
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .send(postDataAstronV2)
+      .expect(200);
+
+    expect(response.body).toHaveProperty("id", queueResponse.body.id); // important to check!
+    expect(response.body).toHaveProperty("key", queueResponse.body.key); // important to check!
+    expect(response.body).toHaveProperty("type", "OPEN-ATTESTATION-TYPE-1");
+    expect(Object.keys(response.body).length).toBe(3);
   });
 });
