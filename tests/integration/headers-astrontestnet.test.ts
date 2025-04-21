ad473e79345225ff5da242a74be7eee6089505d3
maxufeng <36349383+maxufeng@users.noreply.github.com>
2025-04-21 15:13:44 +0800

    feat: add astrontestnet network (#64)
    * feat: add astrontestnet network
    
    * fix: fix code formatting issues
    
    * fix: Perform the code format check using the prettier@3.5.3 version
    
    * fix: update astrontestnet network testfile
    
    * fix: Repair the document document-astrontestnet-storage.test.ts
    
    ---------
    
    Co-authored-by: maxufeng <maxufeng@caict.ac.cn>

---
+++ tests/integration/headers-astrontestnet.test.ts
@@ -0,0 +1,80 @@
+import supertest from "supertest";
+import documentAstron from "../fixtures/v2/document-astrontestnet.json";
+import { ERROR_MESSAGE } from "../../netlify/constants";
+
+const API_ENDPOINT = "http://localhost:9999/.netlify/functions/storage";
+const request = supertest(API_ENDPOINT);
+const postData = { document: documentAstron };
+
+const csrfToken = "mock-csrf-token"; // Mocked CSRF token
+const csrfTokenCookie = `csrfToken=${csrfToken}; HttpOnly; Path=/; SameSite=Strict`; // Mocked CSRF cookie
+
+describe("API key check", () => {
+  it("should fail with 400 when API key is not provided", async () => {
+    const response = await request.post("/").send(postData).expect(400);
+    expect(response.text).toBe(ERROR_MESSAGE.API_KEY_INVALID);
+  });
+
+  it("should fail with 400 when incorrect API key is provided", async () => {
+    const response = await request
+      .post("/")
+      .set("x-api-key", "incorrectKey")
+      .send(postData)
+      .expect(400);
+    expect(response.text).toBe(ERROR_MESSAGE.API_KEY_INVALID);
+  });
+});
+
+describe("cors", () => {
+  it("should fail with 500 when origin is unallowed", async () => {
+    await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("Origin", "http://foobar.com")
+      .send(postData)
+      .expect(500);
+  });
+
+  it("should pass with 200 when origin is TradeTrust creator", async () => {
+    await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .set("Origin", "https://creator.tradetrust.io")
+      .send(postData)
+      .expect(200);
+  });
+
+  it("should pass with 200 when origin is TradeTrust testnets website", async () => {
+    await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .set("Origin", "https://dev.tradetrust.io")
+      .send(postData)
+      .expect(200);
+  });
+
+  it("should pass with 200 when origin is TradeTrust production website", async () => {
+    await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .set("Origin", "https://tradetrust.io")
+      .send(postData)
+      .expect(200);
+  });
+  it("should pass with 200 when origin is TradeTrust production website", async () => {
+    await request
+      .post("/")
+      .set("x-api-key", process.env.API_KEY)
+      .set("x-csrf-token", csrfToken)
+      .set("cookie", csrfTokenCookie)
+      .set("Origin", "https://www.tradetrust.io")
+      .send(postData)
+      .expect(200);
+  });
+});
